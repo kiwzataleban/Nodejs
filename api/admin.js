6eb5b3bfddfa0053d13ba283189988bd3727c41f
Pachara004 <147172411+Pachara004@users.noreply.github.com>
2024-08-21 00:03:14 +0700

    ฝ

---
+++ api/admin.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const { conn } = require('../dbconnect');
+const util = require('util');
--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const app = express();
 const userRoutes = require('./api/user');
 const login = require('./api/login');
 const signup = require('./api/signup');
+const admin = require('./api/admin');
 
 app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/login', login);
 app.use('/signup',signup);
+app.use('/admin',admin);
 
 module.exports = { app };

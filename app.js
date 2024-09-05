061889108963f6fe642f10c1c40ae620e0ea05eb
Pachara004 <147172411+Pachara004@users.noreply.github.com>
2024-09-06 03:14:15 +0700

    Revert "sd"
    This reverts commit 42e992e239a1e37d9f20ac55b7c2654c0b59bca0.

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
+// app.js
 const express = require('express');
 const app = express();
 const userRoutes = require('./api/user');
 const login = require('./api/login');
 const signup = require('./api/signup');
 const admin = require('./api/admin');
-const lotto = require('./api/lotto');
 
 app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/login', login);
-app.use('/signup', signup);
-app.use('/admin', admin);
-app.use('/lotto', lotto);
+app.use('/signup',signup);
+app.use('/admin',admin);
 
-module.exports = app;
+module.exports = { app };
